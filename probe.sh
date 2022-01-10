a17cf95d5f8af23e19dad0d74729b71bf31eb7af
Upendra Sahu <upendra.sahu@maplelabs.com>
2022-01-10 17:50:05 +0530

    probe script

--- probe.sh
+++ probe.sh
@@ -1,2 +1,2 @@
-#~/bin/bash
-curl -X POST --fail -H "Content-Type: application/x-ndjson" --data-binary @/usr/share/apm-server/testdata/intake-v2/minimal-service.ndjson localhost:8200/intake/v2/events
+#!/bin/bash
+curl -X POST --fail -H "Content-Type: application/x-ndjson" --data-binary @testdata/intake-v2/minimal-service.ndjson localhost:8200/intake/v2/events
